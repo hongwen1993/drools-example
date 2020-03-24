87400f6f498fde371695997492b036883441b255
hongwen <15700051127@163.com>
2020-03-25 01:27:57 +0800

    Signed-off-by: hongwen <15700051127@163.com>

---
+++ src/test/java/com/github/hongwen1993/example/DroolsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hongwen1993.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
